6132795be995cc02f24a47a4c736427631a1bd48
helen camily <samueldedeux@gmail.com>
2022-04-01 11:12:20 -0300

    1.0.1

--- calculadora.js
+++ calculadora.js
@@ -2,8 +2,10 @@ var somar = require ("./modulos/somar");
 var subtrair = require ("./modulos/subtrair");
 var multiplicar = require ("./modulos/multiplicar");
 var dividir = require ("./modulos/dividir");
+var media = require ("./modulos/media");
 
 console.log(somar(10,5));
 console.log(subtrair(10,8));
 console.log(multiplicar(2,9));
 console.log(dividir(10,10));
+console.log(media(20,90));
