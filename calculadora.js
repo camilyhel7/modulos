b7d36fee402fd84f1280a72fc4e516ffcfed7e0c
Massuelreis <samueldedeux@gmail.com>
2022-04-01 10:33:26 -0300

    1.0.0

---
+++ calculadora.js
@@ -0,0 +1,9 @@
+var somar = require ("./modulos/somar");
+var subtrair = require ("./modulos/subtrair");
+var multiplicar = require ("./modulos/multiplicar");
+var dividir = require ("./modulos/dividir");
+
+console.log(somar(10,5));
+console.log(subtrair(10,8));
+console.log(multiplicar(2,9));
+console.log(dividir(10,10));
